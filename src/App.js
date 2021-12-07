72dede99bf5a55935730e9b594916402bf6048e7
David Kathoh <mudavidkat@gmail.com>
2021-12-07 17:39:17 +0200

    added web3Modal to support different wallet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import * as React from "react";
 import { ethers } from "ethers";
 import './App.css';
-
 import abi from "./utils/songPortal.json";
 import {BounceLoader,ClipLoader} from "react-spinners";
-
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { errorToast,warningToast,successToast } from "./utils/toast";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+require('dotenv').config()
 export default function App() {
 
   const[currAccount,setCurrentAccount] = React.useState("")
@@ -15,60 +19,93 @@ export default function App() {
   const contractAddress = "0x7Fb01B9833266CE2253459a7A9Cca0cf2192D4E3"
   const contractAbi = abi.abi;
   const rinkebyChain = '0x4'; 
+  const infuraId = process.env.REACT_APP_INFURA_ID;
 
 
  const onChangeHandler = event => {
         setTitle(event.target.value);
       };
 
-  const checkIfWalletIsConnected = ()=>{
-    const {ethereum} = window;
-    if(!ethereum){
-      console.log("Make sure you have metamask installed")
-      return
-    }else{
-      console.log("We have Ethereum object",ethereum)
-    }
-     setCorrectNetwork();
+  const checkIfWalletIsConnected = async()=>{
+    const web3Modal = await getWeb3Modal();
+  if ( web3Modal.cachedProvider) {
+    web3Modal.connect().then((provider) => {
+
+      chainListener(provider);
+      setCorrectNetwork(provider)
+    }).catch((error)=>{
+      console.log(error);
+    });
+   
+  }
   }
 
-  const connectWallet = ()=>{
-    const {ethereum} = window;
-    if(!ethereum){
-      alert("Get metamask");
-    }
-    setCorrectNetwork();
+  const getWeb3Modal = async()=>{
+
+    let providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: infuraId 
+        }
+      }
+    };
+   return  new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions // required
+    });
   }
-  const setCorrectNetwork = async()=>{
-    const {ethereum} = window;
-    const chainId = await ethereum.request({ method: 'eth_chainId' });
-    if(chainId !== rinkebyChain){
+
+  async function isNetWorkCorrect(provider){
+    const chainId = await provider.request({ method: 'eth_chainId' });
+    if(chainId === rinkebyChain){
+      return true
+  }
+  return false
+}
+  const connectWallet = async()=>{
+    
+    
+const web3Modal = await getWeb3Modal();
+ web3Modal.connect().then(async(provider)=>{
+  chainListener(provider);
+ await setCorrectNetwork(provider);
+}).catch((msg)=>{
+  console.log(msg.toString())
+   errorToast('Error when connecting ');
+});
+
+  }
+  const setCorrectNetwork = async(provider)=>{
+    if(!await isNetWorkCorrect(provider)){
  try {
- await ethereum.request({
+ await provider.request({
    method: 'wallet_switchEthereumChain',
    params: [{ chainId: rinkebyChain }],
  });
-       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+       const accounts = await provider.request({ method: 'eth_requestAccounts' });
        const account = accounts[0];
        setCurrentAccount(account);
-       getAllsongs();
+       getAllsongs(provider);
+       successToast("Connected to Rinkeby network")
      
 
 } catch (switchError) {
  // This error code indicates that the chain has not been added to MetaMask.
  if (switchError.code === 4902) {
 
-  alert("You don't have rinkeby in your metamask, add it and try again ");
+  errorToast("You don't have rinkeby in your metamask, add it and try again ");
 
  }
- alert("YFailed to switch to Rinkeby network, Switch to rinkeby network Manualy and try again");
+ errorToast("Failed to switch to Rinkeby network, Switch to rinkeby network Manualy and try again");
  // handle other "switch" errors
 }
     }else{
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+       const accounts = await provider.request({ method: 'eth_requestAccounts' });
        const account = accounts[0];
        setCurrentAccount(account);
-       getAllsongs();
+       getAllsongs(provider);
     }
  
 
@@ -76,35 +113,84 @@ export default function App() {
   const addSong = async() => {
   
   try{
-    setLoading(true);
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+     setLoading(true);
+    const web3Modal = await getWeb3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
     const signer = provider.getSigner()
     const songPortalContract =new ethers.Contract(contractAddress,contractAbi,signer);
-    let count = await songPortalContract.getTotalSongs() 
-    console.log("Retrieved toatal number of songs....",count.toNumber())
     console.log(" song title:",title)
     const songTxn = await songPortalContract.addSong(title,{gasLimit:300000});
     console.log("mining...",songTxn.hash);
     await songTxn.wait()
     console.log("mined---", songTxn.hash)
-    
-    // count = await songPortalContract.getTotalsongs()
-    // console.log("Retrieved toatal song count....",count.toNumber())
     setLoading(false);
     setTitle('');
     }catch(error){
     setLoading(false)
-    alert("Oh not so fast, relax and try again later! Maybe after 15 minutes");
+    if(error.code === 4001){
+      errorToast(" Transaction rejected by user");
+    }else{
+      errorToast("Oh not so fast, relax and try again later! Maybe after 15 minutes");
+    }
+   
   }
     
   }
-  async function getAllsongs(){
+
+
+  function chainListener(provider){
+    provider.on("chainChanged", (chainId) => {
+      console.log("chain changed");
+      if(chainId !== rinkebyChain){
+        warningToast('Wrong Network, please connect to Rinkeby network');
+     }else{
+          setCorrectNetwork(provider);
+        }
+    });
+    
+    
+    // Subscribe to provider connection
+    provider.on('connect', ({ chainId }) => {
+     
+      if(chainId !== rinkebyChain){
+        warningToast('Wrong Network, please connect to Rinkeby network');
+      }else{
+          successToast('Connected!');
+          setCorrectNetwork(provider);
+      }
+      
+    });
+
+    provider.on("accountsChanged", async(accounts) => {
+      if(accounts.length===0){
+           (await getWeb3Modal()).clearCachedProvider()
+           setCurrentAccount(null);
+           setAllsongs([])
+           errorToast('disconnected!');
+      }else{
+        setCorrectNetwork(provider);
+      }
+    });
+    
+    // Subscribe to provider disconnection
+    provider.on("disconnect", async(error) => {
+      console.log("on disconnected");
+      errorToast('disconnected!');
+      (await getWeb3Modal()).clearCachedProvider()
+           setCurrentAccount(null);
+           setAllsongs([])
+    });
+    
+  }
+
+ 
+  async function getAllsongs(connection){
 
     try{
 
-      if(window.ethereum){
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new ethers.providers.Web3Provider(connection);
     const signer = provider.getSigner()
     const songPortalContract =new ethers.Contract(contractAddress,contractAbi,signer);
 
@@ -117,22 +203,32 @@ export default function App() {
         title:song.title
       })
     })
-    setAllsongs(songsCleaned);
+    setAllsongs(songsCleaned.reverse());
 
     //listen to contract emit event when a new song is added
     songPortalContract.on("NewSong",(from,timestamp,title)=>{
       console.log("new song",from,timestamp,title);
+      successToast('Youpi!,new song added')
       setAllsongs(prevState =>[...prevState,{
         from:from,
         title:title,
         timestamp:new Date(timestamp*1000)
       }]);
+
+      toast.success('New song recorded', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+
     });
-     }else{
-        
-      }
+
     }catch(error){
-      console.log(error);
+      errorToast(error);
     }
   }
   React.useEffect(()=>{
@@ -183,6 +279,7 @@ export default function App() {
           })
         }
       </div>
+      <ToastContainer />
     </div>
     
   );
---
+++ src/utils/toast.js
@@ -0,0 +1,37 @@
+import { toast } from 'react-toastify';
+
+
+export function warningToast(message){
+    toast.warn(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      });
+  }
+export  function successToast(message){
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      });
+  }
+ export function errorToast(message){
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      });
+
+  }
